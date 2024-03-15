30c0c75cee726d63184414ad5950ef9871ea11b9
rodrigoaiz <rodrigoaiz@gmail.com>
2024-03-15 14:25:26 -0600

    menú

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import animations from 'tailwindcss-animated'
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkmode: 'class',
 	theme: {
 		extend: {},
 	},
