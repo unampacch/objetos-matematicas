bf969812806f9edc58cffaf85f264bfad35255cb
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-03-17 01:07:52 -0600

    funciona mate1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(),
+    icon({
+      include:{
+        bi: [
+          'facebook',
+          'twitter-x',
+          'youtube',
+          'instagram',
+        ]
+      }
+    }
+  )]
 });
