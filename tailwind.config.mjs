7e93cd9bbb7a0367e7a2e55ab2430d03de8b337b
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-03-18 11:11:45 -0600

    colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkmode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+			light: 'rgb(var(--honeydew) / <alpha-value>)',
+			red: 'rgb(var(--red-pantone) / <alpha-value>)',
+			lightblue : 'rgb(var(--non-photo-blue) / <alpha-value>)',
+			blueish: 'rgb(var(--cerulean) / <alpha-value>)',
+			darkblue: 'rgb(var(--berkeley-blue) / <alpha-value>)',
+			dark: 'rgb(var(--darkest-blue) / <alpha-value>)',
+		}
+		},
 	},
 	plugins: [animations],
 }
