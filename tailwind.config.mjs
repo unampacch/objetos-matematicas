6dcbc0339a7a094fc93af361cbf637b2bb7af64d
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-03-18 22:32:35 -0600

    funciona el bento ya chingue su madre

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,22 @@
 import animations from 'tailwindcss-animated'
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkmode: 'class',
 	theme: {
+		screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
 		extend: {
 			colors: {
 			light: 'rgb(var(--honeydew) / <alpha-value>)',
 			red: 'rgb(var(--red-pantone) / <alpha-value>)',
 			lightblue : 'rgb(var(--non-photo-blue) / <alpha-value>)',
-			blueish: 'rgb(var(--cerulean) / <alpha-value>)',
+			blue: 'rgb(var(--cerulean) / <alpha-value>)',
 			darkblue: 'rgb(var(--berkeley-blue) / <alpha-value>)',
 			dark: 'rgb(var(--darkest-blue) / <alpha-value>)',
 		}
