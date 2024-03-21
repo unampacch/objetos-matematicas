6a06f2756a1a9dd42aefc9d9e7082b0e1b7c12f8
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-03-20 22:54:01 -0600

    add basehader

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Mates al rescate - CCH, UNAM';
+export const SITE_DESCRIPTION = 'Mates al rescate es un proyecto del Colegio de Ciencias y Humanidades de la UNAM que busca ayudar a estudiantes de preparatoria en el aprendizaje de matemáticas.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
