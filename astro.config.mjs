eaae9f64b62755bdaec5c1c2ff8e2fd6e0658ca8
rodrigoaiz <rodrigoaiz@gmail.com>
2024-04-08 11:03:31 -0600

    analytics vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
+
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(),
     icon({
       include:{
